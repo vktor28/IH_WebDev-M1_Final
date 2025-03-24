3f0ff13a2a3a67a5551cd3128f5cfd6c96033c92
vktor28 <v_bcn@hotmail.com>
2025-03-24 23:42:05 +0100

    home newsletter

--- js/components/projects.js
+++ js/components/projects.js
@@ -7,15 +7,15 @@ async function getProjects() {
     console.log(jsonResponse);
 
     const projectCards = document.querySelector("#projects .cards");
-    jsonResponse.slice(0, 3).forEach((project) => {
+    jsonResponse.slice(1, 4).forEach((project) => {
       const card = document.createElement("div");
       card.classList.add("card");
       card.classList.add("mb-2");
       card.innerHTML = `
               <div class="cardImg" style="background-image:url('${project.image}')"></div>
               <div class="cardBody">
-                <h6>${project.name}</h6>
-                <p>${project.description}</p>
+                <h6 class="text-medium mb-1">${project.name}</h6>
+                <p class="headline-regular mb-4">${project.description}</p>
                 <a href="#">Learn more</a>
               </div>`;
       projectCards.appendChild(card);
