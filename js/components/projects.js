2a0b228bc33a01b5aa33d7588fed459052b8c3db
vktor28 <v_bcn@hotmail.com>
2025-03-24 21:13:47 +0100

    js projects + css

--- js/components/projects.js
+++ js/components/projects.js
@@ -1,26 +1,29 @@
-async function obtainData() {
+async function getProjects() {
   try {
     const response = await fetch(
       "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
     );
     const jsonResponse = await response.json();
+    console.log(jsonResponse);
+
+    const projectCards = document.querySelector("#projects .cards");
+    jsonResponse.slice(0, 3).forEach((project) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.classList.add("mb-2");
+      card.innerHTML = `
+              <div class="cardImg" style="background-image:url('${project.image}')"></div>
+              <div class="cardBody">
+                <h6>${project.name}</h6>
+                <p>${project.description}</p>
+                <a href="#">Learn more</a>
+              </div>`;
+      projectCards.appendChild(card);
+    });
   } catch (err) {
     // Handle error or a rejected Promise
     console.log("Something went wrong!", err);
   }
-
-  const projectCards = document.querySelector("#projects .cards");
-  personajes.forEach((elPerson) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
-              <div class="cardImg"></div>
-              <div class="cardBody">
-                <h6>${name}</h6>
-                <p>${description}</p>
-                <a href="#">Learn more</a>
-              </div>`;
-  });
 }
 
-obtainData();
+getProjects();
