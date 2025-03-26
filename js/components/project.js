13ca2d95956f91a894232d531c26a24754481623
vktor28 <v_bcn@hotmail.com>
2025-03-26 21:36:28 +0100

    dynamic projects OK git push

--- js/components/project.js
+++ js/components/project.js
@@ -2,17 +2,15 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 console.log(id);
 async function getProject() {
+  try {
     const response = await fetch(
       "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
     );
     const jsonResponse = await response.json();
-  
+
     const project = jsonResponse.find((item) => item.uuid === id);
-  
-    if (!project) {
-      console.error("No se encontró ningún proyecto con ese ID.");
-      return;
-    }const text = `
+
+    const text = `
     <h1>${project.name}</h1>
     <div class="row">
       <div class="type">${project.description}</div>
@@ -22,11 +20,44 @@ async function getProject() {
     </div>
     <div class="description mb-12">${project.content}</div>
   `;
-
-  // Insertarlo en el DOM
-  const container = document.querySelector("#project .container");
-  container.innerHTML = text;
-
+    const container = document.querySelector("#project .container");
+    container.innerHTML = text;
+  } catch (err) {
+    console.log("Something went wrong!", err);
+  }
 }
 
 getProject();
+
+
+async function getProjects() {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
+      );
+      const jsonResponse = await response.json();
+      const jsonFiltered = jsonResponse.filter(
+        (project) => project.uuid !== id);;
+      console.log(jsonFiltered);
+  
+      const projectCards = document.querySelector("#projects .cards");
+      jsonFiltered.forEach((project) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.classList.add("mb-2");
+        card.innerHTML = `
+                <div class="cardImg" style="background-image:url('${project.image}')"></div>
+                <div class="cardBody">
+                  <h6 class="text-medium mb-1">${project.name}</h6>
+                  <p class="headline-regular mb-4">${project.description}</p>
+                  <a href="/pages/project.html?id=${project.uuid}">Learn more</a>
+                </div>`;
+        projectCards.appendChild(card);
+      });
+    } catch (err) {
+      // Handle error or a rejected Promise
+      console.log("Something went wrong!", err);
+    }
+  }
+  getProjects();
+  
