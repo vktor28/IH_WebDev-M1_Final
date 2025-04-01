9127c9b3d1d3f56f4e690c91af44a166a5ada683
vktor28 <v_bcn@hotmail.com>
2025-04-01 21:27:08 +0200

    menu without margin

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,14 @@
-  const toggleLabel = document.querySelector('.nav-toggle-label');
-  const navBar = document.querySelector('.nav-bar');
+const toggleLabel = document.querySelector(".nav-toggle-label");
+const navBar = document.querySelector(".nav-bar");
 
-  toggleLabel.addEventListener('click', () => {
-    navBar.classList.toggle('active');
-  });
+toggleLabel.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  toggleLabel.classList.toggle("active");
+});
 
-  document.querySelectorAll('.nav-bar a').forEach(link => {
-    link.addEventListener('click', () => {
-      navBar.classList.remove('active');
-    });
+document.querySelectorAll(".nav-bar a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    toggleLabel.classList.remove("active");
   });
+});
