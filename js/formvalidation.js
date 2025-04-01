6718c049d98e695c475ca70821c14841ac3fb76a
vktor28 <v_bcn@hotmail.com>
2025-04-01 20:59:23 +0200

    js formvalidation new

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -2,10 +2,10 @@ const form = document.getElementById("miFormulario");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(event.target[0].value);
-  console.log(event.target[1].value);
-  console.log(event.target[2].value);
-  console.log(event.target[3].value);
+  console.log("name: " + event.target[0].value);
+  console.log("email: " + event.target[1].value);
+  console.log("phone: " + event.target[2].value);
+  console.log("message: " + event.target[3].value);
   if (event.target[0].value == "ironhack") {
     alert("Name must be diferent than ironhack");
     return false;
